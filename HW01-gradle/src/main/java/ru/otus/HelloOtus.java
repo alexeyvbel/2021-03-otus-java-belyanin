9d852cf61f588ee09eb13cf5ded25b8c1a3b305c
Belaynin Alexey <alexeyvbel@gmail.com>
2021-04-04 23:13:20 +0300

    hw01-gradle
    hw01-gradle
    
    hw01-gradle

---
+++ HW01-gradle/src/main/java/ru/otus/HelloOtus.java
@@ -0,0 +1,21 @@
+package ru.otus;
+
+import com.google.common.collect.Lists;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HelloOtus {
+
+    public void sayHelloOtus() {
+        List<Integer> example = new ArrayList<>();
+        int min = 0;
+        int max = 100;
+        for (int i = min; i < max; i++) {
+            example.add(i);
+        }
+
+        System.out.println(Lists.reverse(example));
+
+    }
+}
