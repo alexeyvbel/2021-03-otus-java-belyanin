9763907cff9d52b70d60bf7c2d5b5f63812b9168
Belaynin Alexey <alexeyvbel@gmail.com>
2021-05-31 23:33:00 +0300

    HW03-annotations

---
+++ HW03-annotations/src/main/java/ru/otus/main/main.java
@@ -0,0 +1,11 @@
+package ru.otus.main;
+
+public class main {
+
+    public static void main(String[] args) {
+        Class<?> classTest = runTest.class;
+        Class<?> classBefore = runTest.class;
+        Class<?> classAfter = runTest.class;
+
+    }
+}
---
+++ HW03-annotations/src/main/java/ru/otus/main/runTest.java
@@ -0,0 +1,23 @@
+package ru.otus.main;
+
+import ru.otus.annotations.After;
+import ru.otus.annotations.Before;
+import ru.otus.annotations.Test;
+
+public class runTest {
+
+    @Test
+    public void doTest(){
+
+    }
+
+    @Before
+    public void doBefore(){
+
+    }
+
+    @After
+    public void doAfter(){
+
+    }
+}
