9f07b2a259497dc07a131c107982b8b4ba5d106d
Alexey <34470819+alexeyvbel@users.noreply.github.com>
2022-01-08 12:22:18 +0300

    HW08-json (#9)
    HW08-json
    
    HW08-json
    
    HW08-json fix filePath
    
    HW08-json fix ru/otus

--- buildSrc/src/main/java/Versions.java
+++ buildSrc/src/main/java/Versions.java
@@ -1,3 +1,11 @@
 public interface Versions {
     String guava = "28.2-jre";
+    String jmh = "1.32";
+    String jol = "0.16";
+    String asm = "9.1";
+    String glassfishJson = "1.1.4";
+    String protobuf = "3.12.1";
+
+    String lombok = "1.18.22";
+    String gson = "2.8.8";
 }
